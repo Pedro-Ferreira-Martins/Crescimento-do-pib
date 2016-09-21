821c18dc9f829f86d56be29eacd7629dbf0462b1
Pedro-Ferreira-Martins <emaildopedro.fm@gmail.com>
2016-09-21 15:18:03 -0300

    função funcionando

--- crescimento_PIB.py
+++ crescimento_PIB.py
@@ -2,17 +2,20 @@
 
 
 def Maior_Crescimento(a):
-	num=range(len(a)-2)
-	x={}#{porcentagem de a[i]:a[i]}
-	for i in num:
-		dif_=a[i]-a[i+2]
-		porcentagem=dif_/(a[i]/100)
-		x[-porcentagem]=a[i]
-	print(x)
-	b=max(x)
-	ano1=a.index(x[b])+1
-	ano2=ano1+2
-	print('A maior media de crescimento foi entre os anos %d e %d: %d'%(ano1,ano2,b))
+        num=range(len(a))
+        x=[]#{ano:% de cresc}
+        for i in num:
+                if i==0:
+                        x.append(0)
+                else:
+                        x.append((a[i]-a[i-1])/a[i-1]*100)
+        trienios=[]
+        num=range(len(a)-2)
+        for i in num:
+                trienios.append((x[i]+x[i+1]+x[i+2])/3)
+        maior=max(trienios)
+        n_ano=trienios.index(maior)
+        print('A maior media de crescimento foi entre os anos %d e %d: %.1f'%(n_ano,n_ano+2,maior))
 
-casa=[4.0,1.72,3.67,9.66,3.05]
+casa=[1.89,7.91,1.24,7.92,8.79]
 Maior_Crescimento(casa)
